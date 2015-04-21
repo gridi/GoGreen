21cd547fecf15397c927067326235bbd8322d030
Linda van der Horst <linda.van.der.horst@ordina.nl>
2015-04-21 16:40:43 +0200

    colorfield heeft de parameter color gekregen

--- site/src/main/java/com/onehippo/gogreen/components/Colorfield.java
+++ site/src/main/java/com/onehippo/gogreen/components/Colorfield.java
@@ -2,27 +2,21 @@ package com.onehippo.gogreen.components;
 
 import org.hippoecm.hst.core.component.HstRequest;
 import org.hippoecm.hst.core.component.HstResponse;
+import org.hippoecm.hst.core.parameters.ParametersInfo;
 
+@ParametersInfo(type = ColorfieldInfo.class)
 public class Colorfield extends BaseComponent {
 
 	@Override
 	public void doBeforeRender(HstRequest request, HstResponse response) {
-
 		super.doBeforeRender(request, response);
 
-		int nummer = (int) (Math.random() * 2);
-		
-		boolean greenText = true;
-		
+		String color = ""; // TODO get it from parameters
 
-			
-		if (nummer == 0){
-			greenText = true;
-		} else {
-			greenText = false;
-		}
-		
-		
-		request.setAttribute("green", greenText);
+		ColorfieldInfo info = getComponentParametersInfo(request);
+		color = info.getColor();
+
+		System.out.println("COLOR = " + color);
+		request.setAttribute("color", color);
 	}
 }
--- site/src/main/webapp/WEB-INF/jsp/common/colorfield.jsp
+++ site/src/main/webapp/WEB-INF/jsp/common/colorfield.jsp
@@ -1,13 +1,3 @@
 <%@include file="../includes/tags.jspf"%>
 
-
-
-
-<c:choose>
-  <c:when test="${green}">
-    <FONT SIZE="6" COLOR="#CCFF66" FACE="Courier New"><b>Gelijk aan 0 in groen.</b></FONT>
-  </c:when>
-  <c:otherwise>
-    <FONT SIZE="6" COLOR="#FF0000" FACE="Courier New"><b>Groter dan 0 in rood.</b></FONT>
-  </c:otherwise>
-</c:choose>
+    <FONT SIZE="6" COLOR="${color}" FACE="Courier New"><b>Het is ${color} .</b></FONT>
